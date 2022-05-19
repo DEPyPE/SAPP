217b47a44c06d02a538fc1a0223a4ca145eba3dc
Wamor17 <wa_moreno@seg.guanajuato.gob.mx>
2022-05-19 11:49:47 -0500

    Codigo bas

--- Resource/js/index.js
+++ Resource/js/index.js
@@ -10,21 +10,45 @@ $(function(){
     $('select').formSelect();
 });
 
+function Get_ProjectData(UserID){
+    var TypDat = "ProjectInfo";
+
+    $.post("Controller/HomeEPP_ReadController.php", {ID: UserID, TypeData: TypDat}, function(DataProject){
+        ProjectData = JSON.parse( DataProject );
+        localStorage.setItem("ProjectInfo", JSON.stringify(ProjectData) );
+
+        console.log( "Project information => " + ProjectData );
+    });
+}
+
 $('.btn-login').on('click', function(){
     var user = $('#txt_user').val();
     var pass = $('#txt_password').val();
-    console.log(pass)
+    console.log(pass);
 
-    if( user == "walter" && pass == "123" ){
-        window.location = "home_encargado_proyecto.html";
-    }else if( user == "isa" && pass == "123" ){
-        window.location = "home_evaluacion.html";
-    }else if( user == "tabita" && pass == "123" ){
-        window.location = "home_planeacion.html";
-    }else{
-        M.toast({html: 'Correo o contraseña incorrectos', classes: 'rounded'});
-    }
+    $.post("Controller/IndexController.php", {NameUser: user, Password: pass}, function( Data ) {
+        var UserData = JSON.parse( Data );
 
-    $('#txt_user').val("");
-    $('#txt_password').val("");
+        if( UserData.Status == "Correct" ){
+            localStorage.setItem("UserData", JSON.stringify(UserData) );
+            Get_ProjectData( UserData.ID_Usuario );
+            
+            M.toast({html: '¡Bienvenido!', classes: 'green rounded'});
+
+            setTimeout( function(){
+                window.location = UserData.URL;
+            }, 1000 );
+
+        }else if( UserData.Status == "More users" ){
+            M.toast({html: 'Existen más de un usuario registrado <br> con el mismo correo', classes: 'red lighten-1 rounded center-align'});
+
+        }else{
+            M.toast({html: 'Correo o contraseña incorrectos', classes: 'red rounded'});
+        }
+
+        console.log( "User information => " + UserData );
+    });
+
 });
+
+
